f0e53411415d23e706782f798a4543fa38d45612
robiul99siyam <siyamrobiulislam@gmail.com>
2024-10-23 20:40:29 +0600

    project run

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import av from "./assets/cart-item.png"
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-xl text-center text-colors'>Hello World in React.js</h1>
+      <img src={av} alt="" />
+    </div>
+  )
+}
